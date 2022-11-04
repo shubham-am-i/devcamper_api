c6d287870f5876633dc19b0c2178a7128f1cbe21
shubham-am-i <patilshubham2k21@gmail.com>
2022-11-04 12:27:32 +0530

    connect to a database with mongoose

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import morgan from 'morgan'
-
-// Route files
-import bootcamps from './routes/bootcamps.js'
+import connectDB from './config/db.js'
 
 // Load env vars
 dotenv.config({ path: './config/config.env' })
 
+// Connect to Atlas
+connectDB()
+
+// Route files
+import bootcamps from './routes/bootcamps.js'
 const app = express()
 
 // Middlewares
@@ -18,7 +21,17 @@ if (process.env.NODE_ENV === 'development') {
 app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 )
+
+// Handle unhandled promise rejections
+// If our database is isn't connected, we don't want our app to even run
+// so we are crashing over server, instead of using try-catch in db.js
+
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`)
+  // close server & exit process
+  server.close(() => process.exit(1))
+})
