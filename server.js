45ea0a0f6ea41f4f6ea1da5fb9ec7e0c78adf658
shubham-am-i <patilshubham2k21@gmail.com>
2022-11-04 09:57:45 +0530

    using the express router

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 import express from 'express'
 import dotenv from 'dotenv'
 
+// Route files
+import bootcamps from './routes/bootcamps.js'
+
 // Load env vars
 dotenv.config({ path: './config/config.env' })
 
 const app = express()
 
-const PORT = process.env.PORT || 5000
+// Mount Routers
+app.use('/api/v1/bootcamps', bootcamps)
 
+const PORT = process.env.PORT || 5000
 app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
