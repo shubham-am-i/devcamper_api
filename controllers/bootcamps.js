191bff33e89b3fdc7ac830131c667ad1de0f1d65
shubham-am-i <patilshubham2k21@gmail.com>
2022-11-05 08:39:58 +0530

    add Mongoose Error Handling

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -17,16 +17,16 @@ export const getBootcamp = async (req, res, next) => {
   try {
     const bootcamp = await Bootcamp.findById(req.params.id)
     if (!bootcamp) {
-      return res.status(400).json({ success: false })
+      return next(
+        new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404)
+      )
     }
     res.status(200).json({
       success: true,
       data: bootcamp,
     })
   } catch (err) {
-    next(
-      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
-    )
+    next(err)
   }
 }
 
