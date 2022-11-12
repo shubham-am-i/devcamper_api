60bb95b4a14a26e4eb2bd3b08c2f269ace0f0a81
shubham-am-i <patilshubham2k21@gmail.com>
2022-11-12 07:52:39 +0530

    implement update & delete review

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -75,3 +75,53 @@ export const addReview = asyncHandler(async (req, res, next) => {
     data: review,
   })
 })
+
+// @desc    Update review
+// @route   PUT /api/v1/reviews/:id
+// @access  Private
+export const updateReview = asyncHandler(async (req, res, next) => {
+  let review = await Review.findById(req.params.id)
+
+  if (!review) {
+    return next(
+      new ErrorResponse(`No Review found with the id of ${req.params.id}`, 404)
+    )
+  }
+
+  // Make sure review belongs to user or user is admin
+  if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse(`Not authorized to update review`, 401))
+  }
+
+  review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  })
+  res.status(200).json({
+    success: true,
+    data: review,
+  })
+})
+// @desc    Delete review
+// @route   DELETE /api/v1/reviews/:id
+// @access  Private
+export const deleteReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.findById(req.params.id)
+
+  if (!review) {
+    return next(
+      new ErrorResponse(`No Review found with the id of ${req.params.id}`, 404)
+    )
+  }
+
+  // Make sure review belongs to user or user is admin
+  if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse(`Not authorized to update review`, 401))
+  }
+
+  await review.remove()
+  res.status(200).json({
+    success: true,
+    data: {},
+  })
+})
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -3,6 +3,8 @@ import {
   getReviews,
   getReview,
   addReview,
+  updateReview,
+  deleteReview,
 } from '../controllers/reviewController.js'
 
 const router = express.Router({ mergeParams: true })
@@ -10,6 +12,10 @@ const router = express.Router({ mergeParams: true })
 import { protect, authorize } from '../middleware/authmiddleware.js'
 
 router.route('/').get(getReviews).post(protect, authorize, addReview)
-router.route('/:id').get(getReview)
+router
+  .route('/:id')
+  .get(getReview)
+  .put(protect, authorize, updateReview)
+  .delete(protect, authorize, deleteReview)
 
 export default router
